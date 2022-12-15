fffa4a33d3125785bb9d34f7d45bef107e4a0c4b
Raana Yavari <40792821+raanaYavari@users.noreply.github.com>
2022-12-15 13:26:58 +0330

    feat(navigation) :add navigation graph

--- app/src/main/java/com/raana/bamacodechallenge/ui/MyApp.kt
+++ app/src/main/java/com/raana/bamacodechallenge/ui/MyApp.kt
@@ -1,20 +1,32 @@
 package com.raana.bamacodechallenge.ui
 
+import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.compose.rememberNavController
+import com.raana.bamacodechallenge.ui.navigation.NavigationGraph
 import com.raana.bamacodechallenge.ui.theme.MyTheme
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @InternalCoroutinesApi
 @Composable
-fun MyApp(){
+fun MyApp() {
 
     MyTheme {
         val navController = rememberNavController()
         val coroutineScope = rememberCoroutineScope()
         val scaffoldState = rememberScaffoldState()
+        Scaffold(
+            scaffoldState = scaffoldState,
+        ) {
+            NavigationGraph(
+                navController = navController,
+                coroutineScope = coroutineScope,
+                scaffoldState = scaffoldState
+            )
+        }
     }
+
 
 }
--- app/src/main/java/com/raana/bamacodechallenge/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/raana/bamacodechallenge/ui/navigation/NavigationGraph.kt
@@ -5,18 +5,15 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navDeepLink
 import com.raana.bamacodechallenge.ui.navigation.Screens.DETAIL_ROUTE
 import com.raana.bamacodechallenge.ui.navigation.Screens.HOME_ROUTE
 import com.raana.bamacodechallenge.ui.navigation.Screens.POST_ROUTE
 import com.raana.bamacodechallenge.ui.navigation.Screens.SPLASH_ROUTE
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.InternalCoroutinesApi
 
 
 object Screens {
@@ -26,7 +23,6 @@ object Screens {
     const val POST_ROUTE = "post"
 }
 
-@InternalCoroutinesApi
 @Composable
 fun NavigationGraph(
     navController: NavHostController = rememberNavController(),
