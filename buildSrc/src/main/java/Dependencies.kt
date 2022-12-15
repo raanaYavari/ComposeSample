7f66ad6b0396b36f24ca18076f9c2998d4fc8840
Raana Yavari <40792821+raanaYavari@users.noreply.github.com>
2022-12-15 11:14:39 +0330

    feat(build-src): add dsl build

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,83 @@
+object Dependencies {
+
+    const val ANDROIDX_APPCOMPACT = "androidx.appcompat:appcompat:${Version.ANDROIDX_APPCOMPACT}"
+    const val ANDROIDX_ACTIVITY = "androidx.activity:activity-ktx:${Version.ANDROIDX_ACTIVITY}"
+    const val ANDROIDX_CORE = "androidx.core:core-ktx:${Version.ANDROIDX_CORE}"
+    const val MATERIAL = "com.google.android.material:material:${Version.MATERIAL}"
+
+    // Hilt
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:${Version.HILT}"
+    const val HILT_NAVIGATION = "androidx.hilt:hilt-navigation-compose:${Version.HILT_NAVIGATION}"
+    const val HILT_COMPILER = "com.google.dagger:hilt-compiler:${Version.HILT_COMPILER}"
+
+    // Startup
+    const val STARTUP = "androidx.startup:startup-runtime:${Version.STARTUP}"
+
+    // Coroutine
+    const val COROUTINE_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.COROUTINE}"
+    const val COROUTINE_ANDROID =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Version.COROUTINE}"
+    const val LIFECYCLE_LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:${Version.LIFECYCLE}"
+    const val LIFECYCLE_VIEWMODEL =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.LIFECYCLE}"
+    const val LIFECYCLE_SAVEDSTATE =
+        "androidx.lifecycle:lifecycle-viewmodel-savedstate:${Version.LIFECYCLE}"
+    const val LIFECYCLE_VIEWMODEL_COMPOSE =
+        "androidx.lifecycle:lifecycle-viewmodel-compose:${Version.LIFECYCLE}"
+
+    // Compose
+    const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Version.COMPOSE}"
+    const val COMPOSE_RUNTIME = "androidx.compose.runtime:runtime:${Version.COMPOSE}"
+    const val COMPOSE_UI = "androidx.compose.ui:ui:${Version.COMPOSE}"
+    const val COMPOSE_FOUNDATION_LAYOUT =
+        "androidx.compose.foundation:foundation-layout:${Version.COMPOSE}"
+    const val COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:${Version.COMPOSE}"
+    const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Version.COMPOSE}"
+    const val COMPOSE_MATERIAL_ICONS =
+        "androidx.compose.material:material-icons-extended:${Version.COMPOSE}"
+    const val COMPOSE_ANIMATION = "androidx.compose.animation:animation:${Version.COMPOSE}"
+    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:${Version.COMPOSE}"
+    const val COMPOSE_RUNTIME_LIVEDATA =
+        "androidx.compose.runtime:runtime-livedata:${Version.COMPOSE}"
+
+    // Accompanist
+    const val ACCOMPANIST_PAGER = "com.google.accompanist:accompanist-pager:${Version.ACCOMPANIST}"
+    const val ACCOMPANIST_SWIPE_REFRESH =
+        "com.google.accompanist:accompanist-swiperefresh:${Version.ACCOMPANIST}"
+    const val ACCOMPANIST_INSETS =
+        "com.google.accompanist:accompanist-insets:${Version.ACCOMPANIST}"
+    const val ACCOMPANIST_SYSTEM_UI_CONTROLLER =
+        "com.google.accompanist:accompanist-systemuicontroller:${Version.ACCOMPANIST}"
+    const val ACCOMPANIST_FLOW_LAYOUT =
+        "com.google.accompanist:accompanist-flowlayout:${Version.ACCOMPANIST}"
+    const val ACCOMPANIST_PERMISSIONS =
+        "com.google.accompanist:accompanist-permissions:${Version.ACCOMPANIST}"
+
+    // Navigation
+    const val NAVIGATION_COMPOSE =
+        "androidx.navigation:navigation-compose:${Version.NAVIGATION_COMPOSE}"
+
+    // Retrofit
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Version.RETROFIT}"
+    const val RETROFIT_CONVERTER_SCALARS =
+        "com.squareup.retrofit2:converter-scalars:${Version.RETROFIT}"
+    const val LOGGING_INTERCEPTOR =
+        "com.squareup.okhttp3:logging-interceptor:${Version.LOGGING_INTERCEPTOR}"
+
+    // Timber
+    const val TIMBER = "com.jakewharton.timber:timber:${Version.TIMBER}"
+
+
+    // Serialization
+    const val KOTLIN_X_SERIALIZATION =
+        "org.jetbrains.kotlinx:kotlinx-serialization-json:${Version.KOTLIN_X_SERIALIZATION}"
+    const val KOTLIN_SERIALIZATION_CONVERTOR =
+        "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:${Version.KOTLIN_SERIALIZATION_CONVERTOR}"
+
+    // DataStore
+    const val DATA_STORE = "androidx.datastore:datastore-preferences:${Version.DATA_STORE}"
+
+    //play services
+    const val PLAY_SERVICES_AUTH = "com.google.android.gms:play-services-auth:${Version.PLAY_SERVICES}"
+
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,50 @@
+object Version {
+
+    const val ANDROIDX_APPCOMPACT = "1.4.1"
+    const val ANDROIDX_ACTIVITY = "1.4.0"
+    const val ANDROIDX_CORE = "1.7.0"
+    const val MATERIAL = "1.5.0"
+
+    // Hilt
+    const val HILT = "2.41"
+    const val HILT_NAVIGATION = "1.0.0"
+    const val HILT_COMPILER = "2.41"
+
+    // Startup
+    const val STARTUP = "1.1.1"
+
+    // Coroutine
+    const val COROUTINE = "1.6.0-native-mt"
+    const val LIFECYCLE = "2.4.1"
+
+    // Compose
+    const val COMPOSE = "1.2.0-alpha06"
+    const val ACTIVITY_COMPOSE = "1.4.0"
+
+    // Accompanist
+    const val ACCOMPANIST = "0.22.0-rc"
+
+    // Navigation
+    const val NAVIGATION_COMPOSE = "2.4.1"
+
+    // Retrofit
+    const val RETROFIT = "2.9.0"
+    const val LOGGING_INTERCEPTOR = "5.0.0-alpha.3"
+
+    // Timber
+    const val TIMBER = "5.0.1"
+
+    // Kotlin Serialization
+    const val KOTLIN_X_SERIALIZATION = "1.3.2"
+    const val KOTLIN_SERIALIZATION_CONVERTOR = "0.8.0"
+
+    // DataStore
+    const val DATA_STORE = "1.0.0"
+
+    //Play Services Auth
+    const val PLAY_SERVICES = "20.3.0"
+
+
+
+
+}
