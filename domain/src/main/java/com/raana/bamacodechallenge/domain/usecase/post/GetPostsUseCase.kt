d9e2dbb38be46a11c6674862beac659dcb3b3758
Raana Yavari <40792821+raanaYavari@users.noreply.github.com>
2022-12-16 09:33:18 +0330

    feat(di) : add repo & services di

--- domain/src/main/java/com/raana/bamacodechallenge/domain/usecase/post/GetPostsUseCase.kt
+++ domain/src/main/java/com/raana/bamacodechallenge/domain/usecase/post/GetPostsUseCase.kt
@@ -2,8 +2,9 @@ package com.raana.bamacodechallenge.domain.usecase.post
 
 import com.raana.bamacodechallenge.domain.repository.post.PostRepository
 import com.raana.bamacodechallenge.domain.repository.post.model.Post
+import javax.inject.Inject
 
-class GetPostsUseCase constructor(private val postRepository: PostRepository) {
+class GetPostsUseCase @Inject constructor(private val postRepository: PostRepository) {
 
     suspend fun invoke(): Result<List<Post>> {
         return postRepository.getPosts()
--- domain/src/main/java/com/raana/bamacodechallenge/domain/usecase/user/GetUsersUseCase.kt
+++ domain/src/main/java/com/raana/bamacodechallenge/domain/usecase/user/GetUsersUseCase.kt
@@ -2,8 +2,9 @@ package com.raana.bamacodechallenge.domain.usecase.user
 
 import com.raana.bamacodechallenge.domain.repository.user.UserRepository
 import com.raana.bamacodechallenge.domain.repository.user.model.User
+import javax.inject.Inject
 
-class GetUsersUseCase constructor(private val userRepository: UserRepository) {
+class GetUsersUseCase @Inject constructor(private val userRepository: UserRepository) {
 
     suspend fun invoke(): Result<List<User>> {
         return userRepository.getUsers()
