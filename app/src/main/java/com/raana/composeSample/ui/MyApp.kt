b39346e05515240d61e848c2b70a8ef8ab49219f
raana <raanayavari@gmail.com>
2023-11-01 10:21:19 +0330

    fix MyApp.kt

--- app/src/main/java/com/raana/composeSample/ui/MyApp.kt
+++ app/src/main/java/com/raana/composeSample/ui/MyApp.kt
@@ -6,32 +6,35 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.raana.composeSample.ui.component.AppBottomNavigationBar
 import com.raana.composeSample.ui.navigation.NavigationGraph
+import com.raana.composeSample.ui.theme.MyTheme
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @InternalCoroutinesApi
 @Composable
 fun MyApp() {
 
-    val navController = rememberNavController()
-    val scaffoldState = rememberScaffoldState()
-    Scaffold(
-        scaffoldState = scaffoldState,
-        bottomBar = {
-            AppBottomNavigationBar(navController) {
-                navController.navigate(it.route) {
-                    navController.graph.startDestinationRoute?.let { route ->
-                        popUpTo(route) {
-                            saveState = true
+    MyTheme {
+        val navController = rememberNavController()
+        val scaffoldState = rememberScaffoldState()
+        Scaffold(
+            scaffoldState = scaffoldState,
+            bottomBar = {
+                AppBottomNavigationBar(navController) {
+                    navController.navigate(it.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
                         }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
                 }
             }
+        ) {
+            NavigationGraph(
+                navController = navController,
+            )
         }
-    ) {
-        NavigationGraph(
-            navController = navController,
-        )
     }
 }
